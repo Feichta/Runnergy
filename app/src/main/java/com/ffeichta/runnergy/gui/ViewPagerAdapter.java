89619bb0b293062ded74b4fa230b84dbe3e56bfd
Feichta <fabian.feichter@ffeichta.com>
2015-12-28 17:41:04 +0100

    Going back to the old version of the Tab Layout

--- app/src/main/java/com/ffeichta/runnergy/gui/ViewPagerAdapter.java
+++ app/src/main/java/com/ffeichta/runnergy/gui/ViewPagerAdapter.java
@@ -1,6 +1,5 @@
 package com.ffeichta.runnergy.gui;
 
-import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -10,18 +9,17 @@ import android.support.v4.app.FragmentStatePagerAdapter;
  */
 public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
-    final int PAGE_COUNT = 2;
-    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
-    private Context context;
+    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
 
-    public ViewPagerAdapter(FragmentManager fm, Context context) {
+
+    // Build a Constructor and assign the passed Values to appropriate values in the class
+    public ViewPagerAdapter(FragmentManager fm, CharSequence mTitles[], int mNumbOfTabsumb) {
         super(fm);
-        this.context = context;
-    }
 
-    @Override
-    public int getCount() {
-        return PAGE_COUNT;
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+
     }
 
     //This method return the fragment for the every position in the View Pager
@@ -37,18 +35,19 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
             Tab2 tab2 = new Tab2();
             return tab2;
         }
-
-
     }
 
     // This method return the titles for the Tabs in the Tab Strip
 
     @Override
     public CharSequence getPageTitle(int position) {
-        // Generate title based on item position
-        return tabTitles[position];
+        return Titles[position];
     }
+
     // This method return the Number of tabs for the tabs Strip
 
-
+    @Override
+    public int getCount() {
+        return NumbOfTabs;
+    }
 }
