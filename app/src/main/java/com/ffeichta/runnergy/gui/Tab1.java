7d1f2d03ad60796d4d1e90d94735ec93a7c13c1f
Feichta <fabian.feichter@ffeichta.com>
2015-12-28 10:02:21 +0100

    Added sample code for tabbed layout activity

---
+++ app/src/main/java/com/ffeichta/runnergy/gui/Tab1.java
@@ -0,0 +1,26 @@
+package com.ffeichta.runnergy.gui;
+
+/**
+ * Created by Fabian on 28.12.2015.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ffeichta.runnergy.R;
+
+/**
+ * Created by hp1 on 21-01-2015.
+ */
+public class Tab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_1, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/ffeichta/runnergy/gui/Tab2.java
@@ -0,0 +1,23 @@
+package com.ffeichta.runnergy.gui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ffeichta.runnergy.R;
+
+/**
+ * Created by Fabian on 28.12.2015.
+ */
+
+
+public class Tab2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_2, container, false);
+        return v;
+    }
+}
