382f7e082df56e600c922f67458cd7941c35f3ba
Feichta <fabian.feichter@ffeichta.com>
2016-02-04 16:21:15 +0100

    ExpandableListView in 'ActivitiesActivity' is working

--- app/src/main/java/com/ffeichta/runnergy/gui/activities/ActivitiesActivity.java
+++ app/src/main/java/com/ffeichta/runnergy/gui/activities/ActivitiesActivity.java
@@ -3,9 +3,12 @@ package com.ffeichta.runnergy.gui.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import com.ffeichta.runnergy.R;
 import com.ffeichta.runnergy.gui.adapter.ActivityAdapter;
@@ -13,22 +16,32 @@ import com.ffeichta.runnergy.model.DBAccessHelper;
 import com.ffeichta.runnergy.model.Track;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Fabian on 29.12.2015.
  */
 public class ActivitiesActivity extends Activity {
     // UI Widgets
-    private ListView listView = null;
 
+
+    List<String> titles = null;
+    // private ArrayList<com.ffeichta.runnergy.model.Activity> activities = null;
+    List<com.ffeichta.runnergy.model.Activity> activities = null;
+    Map<String, ArrayList<com.ffeichta.runnergy.model.Activity>> laptopCollection = null;
+    ExpandableListView expListView = null;
     private Track track = null;
-    private ArrayList<com.ffeichta.runnergy.model.Activity> activities = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activities_activity);
-        listView = (ListView) findViewById(R.id.listViewActivities);
+
+        expListView = (ExpandableListView) findViewById(R.id.listViewActivities);
+
 
         Intent intent = getIntent();
         track = (Track) intent.getSerializableExtra("track");
@@ -39,18 +52,58 @@ public class ActivitiesActivity extends Activity {
             activities = new ArrayList<>();
         }
 
+        titles = new ArrayList<>();
+
+        laptopCollection = new LinkedHashMap<>();
+
+        for (com.ffeichta.runnergy.model.Activity a : activities) {
+            if (!titles.contains(a.getType().toString())) {
+                titles.add(a.getType().toString());
+            }
+        }
+
+        for (String title : titles) {
+            ArrayList<com.ffeichta.runnergy.model.Activity> temp = new ArrayList<>();
+            for (com.ffeichta.runnergy.model.Activity a : activities) {
+                if (a.getType().toString().equals(title)) {
+                    temp.add(a);
+                }
+            }
+            laptopCollection.put(title, temp);
+        }
+
+        for (String s : titles) {
+            Log.d("#####0", s);
+        }
+        Log.d("####", Arrays.toString(laptopCollection.entrySet().toArray()));
+        final ExpandableListAdapter expListAdapter = new ActivityAdapter(this, titles, laptopCollection);
+        expListView.setAdapter(expListAdapter);
+
+
         // Set the Coodinates for each Activity because the ActivityAdapter needs them
         for (com.ffeichta.runnergy.model.Activity activity : activities) {
             activity.setCoordinates(DBAccessHelper.getInstance(this).getCoordinates(activity));
         }
-        ActivityAdapter activityAdapter = new ActivityAdapter(this, activities);
-        listView.setAdapter(activityAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ActivityAdapter activityAdapter = new ActivityAdapter(this, titles, laptopCollection);
+        expListView.setAdapter(activityAdapter);
+        expListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(ActivitiesActivity.this, MapsActivity.class);
                 intent.putExtra("coordinates", activities.get(position).getCoordinates());
                 startActivity(intent);
+            }
+        });
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                final String selected = (String) expListAdapter.getChild(
+                        groupPosition, childPosition);
+                Toast.makeText(getBaseContext(), selected, Toast.LENGTH_LONG)
+                        .show();
+
+                return true;
             }
         });
     }
--- app/src/main/java/com/ffeichta/runnergy/gui/adapter/ActivityAdapter.java
+++ app/src/main/java/com/ffeichta/runnergy/gui/adapter/ActivityAdapter.java
@@ -1,11 +1,13 @@
 package com.ffeichta.runnergy.gui.adapter;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,23 +15,83 @@ import com.ffeichta.runnergy.R;
 import com.ffeichta.runnergy.model.Activity;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Fabian on 29.12.2015.
  */
-public class ActivityAdapter extends ArrayAdapter<Activity> {
+public class ActivityAdapter extends BaseExpandableListAdapter {
 
-    public ActivityAdapter(android.app.Activity context, ArrayList<Activity> activities) {
-        super(context, R.layout.activity_item, activities);
+    private android.app.Activity context;
+    private Map<String, ArrayList<Activity>> activityCollection;
+    private List<String> activities;
+
+    public ActivityAdapter(android.app.Activity context, List<String> activities,
+                           Map<String, ArrayList<Activity>> activityCollection) {
+        this.context = context;
+        this.activityCollection = activityCollection;
+        this.activities = activities;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public int getGroupCount() {
+        return activities.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return activityCollection.get(activities.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return activities.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return activityCollection.get(activities.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String laptopName = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.group_row,
+                    null);
+        }
+        TextView item = (TextView) convertView.findViewById(R.id.heading);
+        item.setTypeface(null, Typeface.BOLD);
+        item.setText(laptopName);
+        return convertView;
+
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
         View ret = convertView;
         ActivityHolder activityHolder;
-        if (ret == null) {
-            LayoutInflater inflater = ((android.app.Activity) getContext())
-                    .getLayoutInflater();
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.activity_item, null);
             ret = inflater.inflate(R.layout.activity_item, parent, false);
             activityHolder = new ActivityHolder();
             activityHolder.image = (ImageView) ret.findViewById(R.id.image_item);
@@ -41,10 +103,10 @@ public class ActivityAdapter extends ArrayAdapter<Activity> {
         } else {
             activityHolder = (ActivityHolder) ret.getTag();
         }
-        Activity a = getItem(position);
+        Activity a = (Activity) getChild(groupPosition, childPosition);
 
         // The outputs in the TextViews are based on the Settings
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
 
         String unit = sp.getString("unit", "km");
         String dateFormat = sp.getString("date", "dd.MM.yyyy");
@@ -78,6 +140,11 @@ public class ActivityAdapter extends ArrayAdapter<Activity> {
         activityHolder.duration.setText(duration);
         activityHolder.date.setText(date);
         return ret;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
     }
 
     // UI Widgets
